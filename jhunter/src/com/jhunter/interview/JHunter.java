4bc8dfe547bb7b2e812afda3eabe57e76e34371f
MTK06830 <MTK06830@MTK06830-L01.eus.mediatek.inc>
2018-04-05 10:22:36 -0700

    Added: - NumericIQ.base10tobase03() - NumericIQ.findNumberInShortedArrayWithoutArrayLength()

--- jhunter/src/com/jhunter/interview/JHunter.java
+++ jhunter/src/com/jhunter/interview/JHunter.java
@@ -4,12 +4,21 @@ public class JHunter {
      
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String base03No = NumericIQ.base10tobase03(10);
+		System.out.print("NumericIQ.base10tobase03(10): " + base03No);
+	
+		int a[] = {1,2,3,4,5,6,7,8};
+		int res = NumericIQ.findNumberInShortedArrayWithoutArrayLength(a, 1);
+		System.out.print("NumericIQ.findNumberInShortedArrayWithoutArrayLength() : " + res);
+		
 		int arr[][] = {{1,1,1,1,1,1}, 
 				 	   {0,1,1,1,1,1}, 
 				 	   {0,0,0,0,1,1},
 				 	   {0,0,1,1,1,1}, 
 				 	   {0,0,0,0,1,1}};
-		Matrix.find1stLeftMost1FromSortedRowsArray(arr);
+		res = Matrix.find1stLeftMost1FromSortedRowsArray(arr);
+		System.out.print("Matrix.find1stLeftMost1FromSortedRowsArray() : " + res);
 	}
 
 }
