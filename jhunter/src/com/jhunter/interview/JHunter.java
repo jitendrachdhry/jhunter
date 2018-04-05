aec2c405b69ec8dcf6e01d0f89b547a480a9b1cf
Jitendra <jitendra.chaudhary@gmail.com>
2018-04-04 21:25:18 -0700

    JHunter main class created.

---
+++ jhunter/src/com/jhunter/interview/JHunter.java
@@ -0,0 +1,10 @@
+package com.jhunter.interview;
+
+public class JHunter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jhunter/src/com/jhunter/interview/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ram
+ *
+ */
+package com.jhunter.interview;
