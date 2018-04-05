db4a493375a4ad705da6051bb0608fc52a66a0b1
Jitendra <jitendra.chaudhary@gmail.com>
2018-04-05 07:25:54 -0700

    Fixed ArrayIndexOutOfBoundsException. Binary search based on ROW not on Columns.

--- jhunter/src/com/jhunter/interview/JHunter.java
+++ jhunter/src/com/jhunter/interview/JHunter.java
@@ -4,8 +4,11 @@ public class JHunter {
      
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[][] = {{0,0,0,1,1,1}, {0,0,1,1,1,1}, {0,0,0,0,1,1},
-				{0,1,1,1,1,1}, {0,0,0,0,1,1}};
+		int arr[][] = {{1,1,1,1,1,1}, 
+				 	   {0,1,1,1,1,1}, 
+				 	   {0,0,0,0,1,1},
+				 	   {0,0,1,1,1,1}, 
+				 	   {0,0,0,0,1,1}};
 		Matrix.find1stLeftMost1FromSortedRowsArray(arr);
 	}
 
