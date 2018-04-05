53f1ab5ce698f306b90b18214482d32d8f8bc791
Jitendra <jitendra.chaudhary@gmail.com>
2018-04-05 07:33:09 -0700

    Added Tree.

--- jhunter/src/com/jhunter/interview/Matrix.java
+++ jhunter/src/com/jhunter/interview/Matrix.java
@@ -15,17 +15,40 @@ public class Matrix {
 	public static int find1stLeftMost1FromSortedRowsArray(int arr[][]) {
 		if(arr==null  || arr.length == 0) return -1;
 		
-		int resultIdx = -1;
-		for(int i=0; i<arr[0].length;i++) {
-			int startIdx = 0, endIdx=arr.length-1, midIdx=0;
+		/*
+		 * Initialize resultIdx with MAX_VALUE.
+		 * FYI, Array size never be above MAX_VALUE. So we are safe initialize with MAX_VALUE.
+		 * Initialize with MAX_VALUE because if no 1 found in array so that we can return -1.
+		 */
+		int resultIdx = Integer.MAX_VALUE;
+		
+		/*
+		 * First Loop for each row, because rows are in sorted order.
+		 * Second loop for checking each column of given row. Its binary search.
+		 * If I found 1 in middle, than i will check middle-1 for 0.
+		 * If middle-1 is 0 than this is the end point to break inner loop. else move startIdx to midIdx + 1.
+		 * Now I will Initialize resultIdx with middle if its value lower than resultIdx.
+		 */
+		for(int i=0; i<arr.length;i++) {
+			int startIdx = 0, endIdx=arr[i].length-1, midIdx=0;
 			do{
 				midIdx = (startIdx + endIdx) / 2;
-				if(arr[midIdx][i] == 1) {
-					return i;
+				if(arr[i][midIdx] == 1) {
+					if(midIdx == 0 || arr[i][midIdx-1] == 0) { // Check for ArrayIndexOutOfBoundsException 
+						resultIdx = Math.min(midIdx, resultIdx);
+						/*
+						 * Do I need to add resultIdx 0th index check condition?? 
+						 * I think this will be extra overhead. 
+						 * Please advice.
+						 */
+						if(resultIdx == 0) { return resultIdx;} 
+						break;
+					}else 
+						endIdx = midIdx - 1;
 				}else
 					startIdx = midIdx + 1;
-			}while(startIdx<endIdx);
+			}while(startIdx<=endIdx);
 		}
-		return resultIdx;
+		return (resultIdx == Integer.MAX_VALUE)? -1 : resultIdx;
 	}
 }
--- jhunter/src/com/jhunter/interview/TreeIQ.java
+++ jhunter/src/com/jhunter/interview/TreeIQ.java
@@ -1,5 +1,39 @@
 package com.jhunter.interview;
 
-public class TreeIQ {
-
+public class TreeIQ {	
+	class TreeNode{
+		Object info;
+		TreeNode next;
+		
+		public TreeNode(Object info) {
+			this.info = info;
+			next = null;
+		}
+		public Object getInfo() { return info; }
+	}
+	
+	public TreeIQ() { root = null; }
+	TreeNode root;
+	
+	class PairInfo{
+		int start, end;
+		public PairInfo(int start, int end) { this.start = start; this.end = end; }
+		public int getStart() {return start; }
+		public int getEnd() {return end; }
+	}
+	
+	public void insert(PairInfo obj) {
+		if(root == null) {
+			root = new TreeNode(obj);
+			return;
+		}
+		
+	}
+	public void constructTree(int arr[][]) {
+		if(arr == null  || arr.length == 0) return;
+		
+		for(int i=0; i<arr.length; i++) {
+			insert(new PairInfo(arr[i][0], arr[i][1]));
+		}
+	}
 }
