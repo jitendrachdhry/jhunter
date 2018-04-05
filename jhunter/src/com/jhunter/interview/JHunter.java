e2ece7afb3081741e8aeec7212b5bc3988d9d267
MTK06830 <MTK06830@MTK06830-L01.eus.mediatek.inc>
2018-04-05 13:42:34 -0700

    Added: Find range in given range array.

--- jhunter/src/com/jhunter/interview/JHunter.java
+++ jhunter/src/com/jhunter/interview/JHunter.java
@@ -19,6 +19,21 @@ public class JHunter {
 				 	   {0,0,0,0,1,1}};
 		res = Matrix.find1stLeftMost1FromSortedRowsArray(arr);
 		System.out.print("Matrix.find1stLeftMost1FromSortedRowsArray() : " + res);
+		
+		int arrRange[][] = {
+				{3,6},
+				{10,20},
+				{7,9},
+				{1,2},
+				{40,100},
+				{21,30},
+				{31,39}
+				};
+		
+		TreeIQ tIQ = new TreeIQ();
+		int resArr[][] = tIQ.findRangeIndex(arrRange, 0);
+		if(resArr == null) System.out.print("tIQ.findRangeIndex() : NO RANGE FOUND");
+		else System.out.print("tIQ.findRangeIndex() : " + resArr[0][0] + " , " + resArr[0][1]);
 	}
 
 }
--- jhunter/src/com/jhunter/interview/TreeIQ.java
+++ jhunter/src/com/jhunter/interview/TreeIQ.java
@@ -3,11 +3,11 @@ package com.jhunter.interview;
 public class TreeIQ {	
 	class TreeNode{
 		Object info;
-		TreeNode next;
+		TreeNode left, right;
 		
 		public TreeNode(Object info) {
 			this.info = info;
-			next = null;
+			left = right = null;
 		}
 		public Object getInfo() { return info; }
 	}
@@ -15,25 +15,64 @@ public class TreeIQ {
 	public TreeIQ() { root = null; }
 	TreeNode root;
 	
-	class PairInfo{
+	class RangePairInfo{
 		int start, end;
-		public PairInfo(int start, int end) { this.start = start; this.end = end; }
-		public int getStart() {return start; }
-		public int getEnd() {return end; }
+		public RangePairInfo(int start, int end) { this.start = start; this.end = end; }
+		public int getStartRange() {return start; }
+		public int getEndRange() {return end; }
 	}
 	
-	public void insert(PairInfo obj) {
+	public void insertRangeNode(RangePairInfo obj) {
 		if(root == null) {
 			root = new TreeNode(obj);
 			return;
 		}
 		
+		TreeNode nodePtr = root;
+		
+		do {
+			if(obj.getStartRange() <= ((RangePairInfo)nodePtr.info).getStartRange()) {
+				if(nodePtr.left != null) nodePtr = nodePtr.left;
+				else {
+					nodePtr.left = new TreeNode(obj);
+					return;
+				}
+			}else {
+				if(nodePtr.right != null) nodePtr = nodePtr.right;
+				else {
+					nodePtr.right = new TreeNode(obj);
+					return;
+				}
+			}
+		}while(true);
 	}
-	public void constructTree(int arr[][]) {
+	
+    void constructTree(int arr[][]) {
 		if(arr == null  || arr.length == 0) return;
 		
 		for(int i=0; i<arr.length; i++) {
-			insert(new PairInfo(arr[i][0], arr[i][1]));
+			insertRangeNode(new RangePairInfo(arr[i][0], arr[i][1]));
 		}
+	}
+	
+	public int[][] findRangeIndex(int arr[][], int findNo) {
+		if(arr==null || arr.length == 0 )
+			return null;
+		
+		constructTree(arr);
+		
+		TreeNode nodePtr = root;
+		while(nodePtr != null){
+			if( findNo >= ((RangePairInfo)nodePtr.info).getStartRange() && findNo <= ((RangePairInfo)nodePtr.info).getEndRange()) {
+				int res[][] = new int[1][2];
+				res[0][0] = ((RangePairInfo)nodePtr.info).getStartRange();
+				res[0][1] = ((RangePairInfo)nodePtr.info).getEndRange();
+				return res;
+			}else if( findNo < ((RangePairInfo)nodePtr.info).getStartRange())
+				nodePtr = nodePtr.left;
+			else
+				nodePtr = nodePtr.right;
+		}
+		return null;
 	}
 }
